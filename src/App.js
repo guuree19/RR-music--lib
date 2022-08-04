7d612b75c5d76d350adb257f291f4c063aab5298
Abdulaziz Ali <guuree1982@gmail.com>
2022-08-03 20:49:14 -0400

    music search app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react'
 import './App.css';
+import Gallery from './Gallery';
+import SearchBar from './SearchBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let [search,setSearch] = useState('')
+  let [message, setMessage] = useState('search for music!')
+  let [data, setData] =    useState([])
+
+    useEffect(() => {
+      const fetchData = async () => {
+          document.title = `${search} Music`
+          const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
+          const resData = await response.json()
+          if (resData.results.length > 0) {
+              setData(resData.results)
+          } else {
+              setMessage('Not Found')
+          }
+      }
+      fetchData()
+    }, [search])
+
+        return (
+          <div className="App">
+            <SearchBar />
+            {message}
+            <Gallery />
+            
+          </div>
+        );
 }
 
 export default App;
