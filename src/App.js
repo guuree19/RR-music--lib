7d6bb8b0f7b2c72d9122c774b3dfacea861f6997
Abdulaziz Ali <guuree1982@gmail.com>
2022-08-06 14:50:24 -0400

    Music Library refactor by implementing useRef and context

--- src/App.js
+++ src/App.js
@@ -1,35 +1,77 @@
-import {useEffect, useState} from 'react'
+import { useState,useRef} from 'react'
 import './App.css';
 import Gallery from './Gallery';
 import SearchBar from './SearchBar';
+import { DataContext } from './context/DataContext'
+import { SearchContext } from './context/SearchContext';
 
-function App() {
-  let [search,setSearch] = useState('')
-  let [message, setMessage] = useState('search for music!')
-  let [data, setData] =    useState([])
 
-    useEffect(() => {
-      const fetchData = async () => {
-          document.title = `${search} Music`
-          const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
-          const resData = await response.json()
-          if (resData.results.length > 0) {
-              setData(resData.results)
-          } else {
-              setMessage('Not Found')
-          }
+const App = () => {
+  let [searchTerm, setSearchTerm] = useState('')
+  let [data, setData] = useState([])
+  let [message, setMessage] = useState('Search for Music!')
+  let searchInput = useRef('')
+  const API_URL = 'https://itunes.apple.com/search?term='
+
+  const handleSearch = (e, term) =>{
+    e.preventDefault()
+        //fetch data
+    const fetchData = async () => {
+      document.title = `${term} Music`
+      const response = await fetch(API_URL + term)
+      const resData = await response.json()
+      if (resData.results.length > 0) {
+        //set State and Context Value
+        return setData(resData.results)
+      } else {
+        return setMessage('Not Found.')
       }
-      fetchData()
-    }, [search])
+  }
+  fetchData()
+  
+}
 
-        return (
-          <div className="App">
-            <SearchBar />
-            {message}
-            <Gallery />
-            
-          </div>
-        );
+  return (
+    <div className="App">
+      <SearchContext.Provider value={{
+          term: searchInput,
+          handleSearch: handleSearch
+      }}> 
+          <SearchBar />
+      </SearchContext.Provider>
+      {message}
+      <DataContext.Provider value={data} >
+        <Gallery />
+      </DataContext.Provider>
+    </div>
+  )
+    
+    
 }
 
 export default App;
+
+
+
+
+
+
+
+        /*function App() {
+          let [search,setSearch] = useState('')
+          let [message, setMessage] = useState('search for music!')
+          let [data, setData] =    useState([])
+
+            useEffect(() => {
+              const fetchData = async () => {
+                  document.title = `${search} Music`
+                  const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
+                  const resData = await response.json()
+                  if (resData.results.length > 0) {
+                      setData(resData.results)
+                  } else {
+                      setMessage('Not Found')
+                  }
+              }
+              fetchData()
+            }, [search])*/
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,12 +1,15 @@
                 
         import { useState } from 'react'
-        function SearchBar(props) {
-            let [searchTerm, setSearchTerm] = useState('')
+        import { useContext } from 'react'
+        import { SearchContext, } from './context/SearchContext'
+
+        function SearchBar() {
+            let {term, handleSearch} = useContext(SearchContext)
 
             return (
                 <form>
-                    <input type="text" placeholder="Enter a search term here" />
-                    <input type="submit" />
+                    <input ref={term} type="text" placeholder="Search here" />
+                    <button onClick={(e) => handleSearch(e, term.current.value)}>Submit</button> 
                 </form>
             )
         }
